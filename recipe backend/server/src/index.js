c563f210f249b366622eb373478c90fd8918a1b6
itsmecoder321 <lubuntu022@gmail.com>
2025-03-09 00:33:33 +0530

    initial commit

---
+++ recipe backend/server/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://ramankhullar2005:ETfEbGOpsZWUwnB2@recipe-sharing.p0k5m.mongodb.net/?retryWrites=true&w=majority&appName=recipe-sharing",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(
+    console.log("DB connected...")
+  )
+
+app.listen(3001, () => console.log("Server started"));
